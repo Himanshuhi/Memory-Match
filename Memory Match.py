64581bc0148a1f93c725c44cfb4d96c1fcc1e568
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-02-28 22:30:16 +0530

    Update Memory Match.py

--- Memory Match.py
+++ Memory Match.py
@@ -1,15 +1,12 @@
 import random
 
-# Create a list of matching pairs of numbers
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
 random.shuffle(numbers)
 
-# Create a list of cards with matching pairs of numbers
 cards = []
 for number in numbers:
     cards.append({'number': number, 'matched': False})
 
-# Define a function to print the board
 def print_board(cards):
     for i in range(0, len(cards)):
         if cards[i]['matched'] == True:
@@ -19,7 +16,6 @@ def print_board(cards):
         if (i + 1) % 4 == 0:
             print()
 
-# Define a function to check for a match
 def check_match(cards, index1, index2):
     if cards[index1]['number'] == cards[index2]['number']:
         cards[index1]['matched'] = True
@@ -28,18 +24,14 @@ def check_match(cards, index1, index2):
     else:
         return False
 
-# Initialize variables
 num_matches = 0
 player1_score = 0
 player2_score = 0
 
-# Main game loop
 while num_matches < 8:
-    # Print the board
     print_board(cards)
     print()
 
-    # Player 1's turn
     print("Player 1's turn")
     index1 = int(input('Enter the index of the first card to flip(0-15): '))
     index2 = int(input('Enter the index of the second card to flip(0-15): '))
@@ -50,12 +42,10 @@ while num_matches < 8:
     else:
         print('No match.')
     
-    # Print the board
     print_board(cards)
     print()
 
-    # Player 2's turn
-    print("Player 2's turn")
+    print("himanshu turn")
     index1 = int(input('Enter the index of the first card to flip(0-15): '))
     index2 = int(input('Enter the index of the second card to flip(0-15): '))
     if check_match(cards, index1, index2):
@@ -65,7 +55,6 @@ while num_matches < 8:
     else:
         print('No match.')
 
-# Game over
 print('Game over!')
 print('Player 1 score:', player1_score)
 print('Player 2 score:', player2_score)
@@ -75,3 +64,6 @@ elif player2_score > player1_score:
     print('Player 2 wins!')
 else:
     print('Tie game!')
+
+    
+ #to exit press ctrl+c   
